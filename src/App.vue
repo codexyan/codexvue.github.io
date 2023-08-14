96166beea0656cfddb0b78aaca3d0063bc1a242f
codexyan <mdcodeid@gmail.com>
2023-08-14 16:31:10 +0700

    update vue project

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
@@ -35,7 +30,9 @@ nav {
   width: 100%;
   font-size: 12px;
   text-align: center;
-  margin-top: 2rem;
+  background-color: black;
+  padding: 12px 0px;
+  margin-bottom: 20px;
 }
 
 nav a.router-link-exact-active {
@@ -58,7 +55,6 @@ nav a:first-of-type {
 
 @media (min-width: 1024px) {
   header {
-    display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
   }
@@ -74,12 +70,13 @@ nav a:first-of-type {
   }
 
   nav {
-    text-align: left;
+    text-align: center;
     margin-left: -1rem;
     font-size: 1rem;
 
     padding: 1rem 0;
     margin-top: 1rem;
+    margin-bottom: 1rem;
   }
 }
 </style>
