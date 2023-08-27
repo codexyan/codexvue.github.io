9851f24344c74a78b822081cf7bc2660c60ccc1f
codexyan <mdcodeid@gmail.com>
2023-08-28 05:25:07 +0700

    add items && category v1

--- src/App.vue
+++ src/App.vue
@@ -1,121 +1,14 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-import { ref } from "vue";
+import { RouterView } from "vue-router";
 
-// Dropdown
-const show = ref(false)
-function toggleDropdown(){
-  show.value = !show.value
-}
+// Component
+import Navbar from "./components/layouts/Navbar.vue";
 </script>
 
 <template>
   <header>
     <div>
-      <!-- Navigation Bar -->
-      <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
-        <div class="container flex flex-wrap items-center justify-between mx-auto my-2">
-          <!-- Logo -->
-          <a href="index.html" class="flex items-center">
-            <img src="@/assets/img/logo.svg" class="h-8 ml-3 sm:ml-0 sm:h-11" alt="Flowbite Logo" />
-          </a>
-
-          <!-- User Action -->
-          <!-- <div class="md:order-2">
-            <a href="login.html"
-              class="px-8 py-3 mt-2 mr-2 text-base font-medium text-black bg-gray-200 border border-transparent rounded-full hover:bg-gray-300 md:py-2 md:text-sm md:px-8 hover:shadow">
-              Sign In
-            </a>
-            <a href="register.html"
-              class="px-8 py-3 text-base font-medium text-white border border-transparent rounded-full bg-navy hover:bg-navy md:py-2 md:text-sm md:px-8 hover:shadow">
-              Sign Up
-            </a>
-          </div> -->
-
-          <!-- User Info -->
-          <div class="md:order-2">
-            <div class="flex items-center">
-              <div class="mr-2 text-sm font-regular">Halo, Fakhrian</div>
-              <button type="button"
-                class="flex mr-3 bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-                id="user-menu-button" aria-expanded="false" data-dropdown-toggle="toggle" @click="toggleDropdown">
-                <span class="sr-only">Open user menu</span>
-                <img class="w-8 h-8 rounded-full" src="https://ui-avatars.com/api/?name=Galih+Pratama&background=160442&color=fff" alt="user-photo">
-              </button>
-            </div>
-
-            <div
-              class="z-50 fixed right-20 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600"
-              id="dropdown" :class="{ hidden: !show }">
-              <div class="px-4 py-3">
-                <span class="block text-sm text-gray-900 dark:text-white">Fakhrian</span>
-                <span
-                  class="block text-sm text-gray-500 truncate font-regular dark:text-gray-400">fakhrian@gmail.com</span>
-              </div>
-
-              <ul class="py-1" aria-labelledby="dropdown">
-                <li>
-                  <a href="#"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Subscriptions</a>
-                </li>
-                <li>
-                  <a href="#"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
-                </li>
-                <li>
-                  <a href="" @click="logout"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign
-                    out</a>
-                </li>
-              </ul>
-            </div>
-
-            <button data-collapse-toggle="mobile-menu-2" type="button"
-              class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="mobile-menu-2" aria-expanded="false">
-              <span class="sr-only">Open main menu</span>
-              <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="https://www.w3.org/2000/svg">
-                <path fill-rule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clip-rule="evenodd" />
-              </svg>
-              <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="https://www.w3.org/2000/svg">
-                <path fill-rule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clip-rule="evenodd" />
-              </svg>
-            </button>
-          </div>
-
-          <!-- Navigation -->
-          <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
-            <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-regular">
-              <li>
-                <RouterLink to="/"
-                  class="block py-2 pl-3 pr-4 text-white bg-indigo-600 rounded md:bg-transparent md:text-indigo-600 md:p-0 dark:text-white"
-                  aria-current="page">Home</RouterLink>
-              </li>
-              <li>
-                <RouterLink to="/categories"
-                  class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-indigo-600 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                  Categories</RouterLink>
-              </li>
-              <li>
-                <RouterLink to="/pricing"
-                  class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-indigo-600 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                  Pricing</RouterLink>
-              </li>
-              <li>
-                <RouterLink to="/portfolio"
-                  class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-indigo-600 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                  Study
-                  Case</RouterLink>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
+      <Navbar/>
     </div>
   </header>
 
---
+++ src/components/layouts/Logo.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <a href="index.html" class="flex items-center">
+        <img src="@/assets/img/logo.svg" class="h-8 ml-3 sm:ml-0 sm:h-16" alt="Flowbite Logo" />
+    </a>
+</template>
